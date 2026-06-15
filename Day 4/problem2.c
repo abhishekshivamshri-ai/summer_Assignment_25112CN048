0ad4ce2b585ec45093f7c320d77a6d1e2ab56470
Abhishek Shrivastava <abhishekshivamshri@gmail.com>
2026-06-15 17:53:04 +0530

    Add files via upload

---
+++ Day 4/problem2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int nthFibonacci(int n) {
+    
+    if (n <= 1){
+        return n;
+    }
+    
+    return nthFibonacci(n - 1) + nthFibonacci(n - 2);
+}
+int main(){
+    int n = 5;
+    int result =nthFibonacci(n);
+    printf("%d
+", result);
+    return 0;
+}
